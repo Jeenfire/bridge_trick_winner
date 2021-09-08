9ae09c4c6c7a2015cd87469cd704a2719b9a50c9
Jeenfire <36032075+Jeenfire@users.noreply.github.com>
2021-09-08 11:32:21 +0200

    FEAT : PlayCommand.php init with randomTrump and randomTrick functions FEAT : Add Trump option to PlayCommand.php

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,19 +9,17 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class DefaultController extends AbstractController
 {
 
-    private $cards = [
-        "2H","3H","4H","5H","6H","7H","8H","9H","10H","JH","QH","KH","AH",
-        "2D","3D","4D","5D","6D","7D","8D","9D","10D","JD","QD","KD","AD",
-        "2S","3S","4S","5S","6S","7S","8S","9S","10S","JS","QS","KS","AS",
-        "2C","3C","4C","5C","6C","7C","8C","9C","10C","JC","QC","KC","AC"
+    private $values = [
+        "2"=>2, "3"=>3,"4"=>4,"5"=>5,"6"=>6,"7"=>7,"8"=>8,
+        "9"=>9,"10"=>10,"J"=>11,"Q"=>12,"K"=>13,"A"=>14
         ];
-
     /**
      * @param string $play
      * @return JsonResponse
      */
     public function trickWinnersNT(string $play): JsonResponse
     {
+        $cards = explode("-",$play);
         $response = new JsonResponse();
         return $response;
     }
@@ -31,4 +29,6 @@ class DefaultController extends AbstractController
         $response = new JsonResponse();
         return $response;
     }
+
+
 }
